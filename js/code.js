e2fb662773f86337f5d2a97a7ee7a213b51cd6a9
史莱姆 <478889187@qq.com>
2019-04-12 00:43:55 +0800

    更新至 v1.3.0 版本

--- js/code.js
+++ js/code.js
@@ -1,5 +1,51 @@
-import '@pub/css/code.min.css'
-import codeModel from './code-model'
+import '~/static/css/code.css'
+// import codeModel from './code-model'
+const codeModel = {
+  javascript: {
+    // 关键词
+    'keyword': /\b(as|async|await|break|undefined|NaN|false|true|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|set|static|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/ig,
+    // 字符串
+    'string': /('[\s\S]*?')|(`[\s\S]*?`)|((?!class=)`[\s\S]*?`)/g,
+    // 注释
+    'annotation': /(\/\/[\s\S]*?\n)|(\/\/[\s\S]*?.*[^\n])|(\/\*\*)[\n\s\S]*?\*\//ig,
+    // 值
+    'value': /\${\w+}|\b\d+|(\.\.\.)\w+|(\w)+(?=,|\)| (=|\*|\+|-|%)|\n|;| \|\||\W?})/g,
+    // 符号
+    'operator': /((?!\*|&lt)[+\-%](?!>|=|\S+>|\(|\*))|(>=|>=|\/=|\*=|-=|\+=|\+\+|--)/g,
+    // 函数、调用
+    'function': /[_$a-z\xA0-\uFFFF][$\w\xA0-\uFFFF]*(?=\s*\()/ig,
+    // 返回值
+    'return': /\b(:?return)\b/g,
+    // 键
+    'key': /(this|[$_]?\w+(?=\.))/g,
+    // 对象键
+    'key-obj': /\w+(?=:)/g,
+    // 方法
+    'methods': /(\.\S+\(\))/g,
+    // 标签组
+    'html': /&lt[^>]*>|&lt\/[^>]*>/ig
+  },
+  html: {
+    // 标签
+    'label': /(<|&lt;)[^>]*>|(<|&lt;)\/[^>]*>/ig,
+    // 属性
+    'key': /[\w-]+="[\w.\s]+"/g,
+    // 注释
+    'annotation': /&lt;!-- [\s\S]*? -->/g
+  },
+  css: {
+    // 选择器
+    'key': /[.*\w]+[\W]?(?={)/g,
+    // 颜色
+    'color': /(?:rgba|rgb|hsl)\([\s\S]*?\)/ig,
+    // 引用
+    'string': /(src|url)\([\s\S]*?\)/g,
+    // 值
+    'value': /(\w+\d+[\w]+(?!:'))|#\w+|[.]?\d+/g,
+    // 属性
+    'keyword': /[-\w]+(?=:)/g
+  }
+}
 
 export default {
 
@@ -35,38 +81,39 @@ class Code {
    */
   identify () {
     // 开头判断如: "// javascript code-model"
-    let content = this.innerText
+    const that = this
+    let content = that.innerText
     let isModel = /^(\/\/ (?:javascript|css|html) code-model\n)/ig
     let search = isModel.exec(content)
     if (isModel) {
-      this.innerText = content.replace(isModel, '')
+      that.innerText = content.replace(isModel, '')
       let model = /(javascript|css|html)+/i
       if (search) {
         search[0].replace(model, keyword => {
           // 设置类名为 model-语言
-          this.$el.className = 'model-' + keyword
-          this.model = keyword
+          that.$el.className = 'model-' + keyword
+          that.model = keyword
 
-          let html = this.form()
+          let html = that.form()
           // 如果为 HTML 模式
           if (keyword === 'html') {
             // 内嵌 js 处理
             html = html.replace(/&lt;script>[\s\S]*?&lt;\/script>/ig, key => {
-              const results = this.form(key.substring(18, key.length - 38), 'javascript')
+              const results = that.form(key.substring(18, key.length - 38), 'javascript')
               return `&lt;script>${results}\n<span class="html-label">&lt;/script></span>`
             })
             // 内嵌 css 处理
               .replace(/&lt;style>[\s\S]*?&lt;\/style>/ig, key => {
-                const results = this.form(key.substring(17, key.length - 36), 'css')
+                const results = that.form(key.substring(17, key.length - 36), 'css')
                 return `&lt;style>${results}<span class="html-label">&lt;/style></span>`
               })
             // 行内 css 处理
               .replace(/style="[\s\S]*?"/ig, key => {
-                const results = this.form(key.substring(7, key.length - 1), 'css')
+                const results = that.form(key.substring(7, key.length - 1), 'css')
                 return `<span class="html-key">style="<span class="model-css">${results}</span>"</span>`
               })
           }
-          this.line(html)
+          that.line(html)
         })
       }
     }
@@ -124,6 +171,46 @@ class Code {
     }
     htmls += '</ul>'
     this.parseHTML = htmls
+    this.consolePanel(htmls)
+  }
+
+  /**
+   * 控制按钮添加
+   */
+  consolePanel (html) {
+    let consolePanel = document.createElement('div')
+    const that = this
+    consolePanel.className = 'code-console-box'
+
+    // 收起
+    const upCode = document.createElement('i')
+    upCode.className = 'iconfont icon-up'
+    upCode.onclick = () => {
+      console.log(upCode, that.$el)
+    }
+
+    // 添加工具
+    this.tools().forEach(item => {
+      const tool = document.createElement(item.type)
+      tool.className = item.className
+      tool.title = item.title
+      tool.onclick = item.fn
+      tool.innerHTML = item.html || ''
+
+      // 子工具
+      item.child && item.child.forEach(childs => {
+        const child = document.createElement(childs.type)
+        child.className = childs.className
+        child.onclick = childs.fn
+        child.innerHTML = childs.html || ''
+        tool.appendChild(child)
+      })
+      consolePanel.appendChild(tool)
+    })
+
+    // consolePanel.appendChild(upCode)
+    that.$el.parentNode.insertBefore(consolePanel, that.$el)
+    // this.$el.insertBefore(consolePanel)
   }
 
   /**
@@ -131,5 +218,70 @@ class Code {
    */
   display (prase = true) {
     this.$el.innerHTML = prase ? this.parseHTML : this.innerText
+  }
+
+  /**
+   * 工具列表
+   */
+  tools () {
+    const that = this
+    // 工具状态
+
+    return [
+      {
+        type: 'i',
+        title: '编辑',
+        className: 'iconfont icon-luru-xianxing',
+        fn: function() {
+          const span = that.$el.lastChild.getElementsByTagName('span')
+          console.log(span.length)
+          for (let i = 0, len = span.length; i < len; i++) {
+            const item = span[i]
+            if (item.className !== 'line') {
+              item.setAttribute('contenteditable', 'true')
+            }
+          }
+        }
+      },
+      {
+        type: 'i',
+        title: '缩小',
+        className: 'iconfont icon-up',
+        fn: function() {
+          const elClass = that.$el.className
+          const className = ' close'
+          if (elClass.indexOf(className) === -1) {
+            that.$el.className += className
+            this.style.transform = ''
+          } else {
+            that.$el.className = elClass.replace(className, '')
+            this.style.transform = 'rotate(180deg)'
+          }
+        }
+      },
+      {
+        type: 'div',
+        title: '关于',
+        className: 'info iconfont icon-caidan',
+        fn: null,
+        child: [
+          {
+            type: 'div',
+            className: 'info-child',
+            fn: null,
+            html:  `<h3>关于本组件</h3><p>作者：史莱姆</p><p>版本：v1.3.0</p>`
+          }
+        ]
+      },
+      {
+        type: 'i',
+        title: '关闭',
+        className: 'iconfont icon-guanbi-xianxing',
+        fn: function() {
+          const parent = that.$el.parentElement
+          parent.parentElement.removeChild(parent)
+        }
+      }
+    ]
   }
 }
